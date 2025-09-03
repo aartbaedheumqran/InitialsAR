b6fb26ce1644bf82b601854e6b6680cf106fd397
aartbaedheumqran <ateeq.rahman2026@gmail.com>
2025-09-02 22:53:05 -0400

    Update InitialsAR.java

--- InitialsAR.java
+++ InitialsAR.java
@@ -1,16 +1,3 @@
-/*
- * Name: Your Name
- * Date: September 2, 2025
- * Title: Unit 0 - Initials Program
- *
- * Description:
- * This simple Java program prints out the user's initials in a creative
- * ASCII-art style. The initials for this project are "A" and "R". The
- * program uses a series of System.out.println statements to draw the
- * letters using stars and spaces.  Additional comments are included
- * to describe major sections of the code.
- */
-
 public class InitialsAR {
     public static void main(String[] args) {
         // Each line below prints a portion of the stylized initials "A R".
